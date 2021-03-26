be831630c6d34abd84485572ce18ca1ef6cab88d
Arbi0810 <tamilin2012@gmail.com>
2021-03-27 01:32:25 +0400

    1

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+// Query params are: utm_creative={{ad.name}}&utm_campaign={{campaign.name}}&utm_source={{site_source_name}}&campaign_id={{campaign.id}}&adset_id={{adset.id}}&ad_id={{ad.id}}&adset_name={{adset.name}}&fbp=98765678 
+
+require_once dirname(__FILE__) . '/kclient.php';
+$client = new KClient('https://ol3kate.tk/api.php?', 'Ps81XkDjM8ZL6sh1');
+$client->sendAllParams();       // to send all params from page query
+$client->forceRedirectOffer();       // redirect to offer if an offer is chosen
+// $client->param('sub_id_5', '123'); // you can send any params
+// $client->keyword('PASTE_KEYWORD');  // send custom keyword
+// $client->currentPageAsReferrer(); // to send current page URL as click referrer
+// $client->debug();              // to enable debug mode and show the errors
+// $client->execute();             // request to api, show the output and continue
+$client->executeAndBreak();     // to stop page execution if there is redirect or some output
+?>
 <!DOCTYPE html>
 <html lang="en" id="storeden-shop.html">
     <head>
